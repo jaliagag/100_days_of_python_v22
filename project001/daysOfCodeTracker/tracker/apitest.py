8dc1996231578860811ea3979aa0ab23feac05e8
jaliagag <jmfaliaga@gmail.com>
2022-08-05 08:17:56 -0300

    im seeing the commits urls and dates for master branch

---
+++ project001/daysOfCodeTracker/tracker/apitest.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import os
+import json
+from pprint import pprint
+
+token = os.getenv('GITHUB_TOKEN', '...')
+owner = 'jaliagag'
+repo = '100_days_of_python_v22'
+query_url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+headers = {'Authorization': f'token {token}'}
+params = {
+    "author": "jaliagag",
+}
+r = requests.get(query_url, headers=headers, params=params).json()
+#pprint(r)
+#pprint(r[1])
+
+for i in r:
+    print(i['html_url'])
+#    print(i['commit'])
+    print(i['commit']['author']['date'])
+#    print(type(r[i]))
+    #pprint(r[i['html_url']])
+#    pprint(
+#        r[-1]
+#    )
